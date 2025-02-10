65e8af4e6e066f654f3ee3f44f2c0ec618038eca
Pavel Stepanov <impetus@yandex.ru>
2025-02-10 13:27:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Stack<String> stack = new Stack<>();
+        while(true) {
+            String str = in.nextLine();
+            String[] result = str.split(" ");
+            switch (result[0]) {
+                case "push":
+                    stack.push(result[1]);
+                    System.out.println("ok");
+                    continue;
+                case "pop":
+                    System.out.println(stack.pop());
+                    continue;
+                case "back":
+                    System.out.println(stack.peek());
+                    continue;
+                case "size":
+                    System.out.println(stack.size());
+                    continue;
+                case "clear":
+                    stack.clear();
+                    System.out.println("ok");
+                    continue;
+                case "exit":
+                    System.out.println("bye");
+                    System.exit(0);
+            }
+        }
+
+    }
+}
